56d67f174a60422e69657a05b02f24cabe072ae7
caden.pei <caden.pei@endeavorstreaming.com>
2026-04-24 16:06:14 +0800

    fix: ci/cd

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/practice-web/'),
+  history: createWebHistory(),
   routes: [],
 })
 
